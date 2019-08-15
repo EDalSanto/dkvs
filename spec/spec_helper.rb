e822b14d1d32e54c21a4c17d6bee9be966fa4fa4
Edward Dal Santo <ejds001@gmail.com>
2019-08-15 15:16:28 -0700

    fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
 STORE_TEST_PATH = "/tmp/test_store"
+require_relative "../lib/server"
+
+class TestServer < Server
+  def primary
+    false
+  end
+
+  def file_path
+    "/tmp/dkvs-test-store"
+  end
+
+  def socket_path
+    "/tmp/dkvs-test.sock"
+  end
+end
